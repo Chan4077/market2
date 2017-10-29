4323679604a89c3ab19eef255590a9a852045a8b
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 01:25:41 +0800

    refactor: Process of converting to mat prefix [9]
    - Add JSDoc to function
    - Restructure grammar

--- src/app/services/urldialog.service.ts
+++ src/app/services/urldialog.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import { MdSnackBar, MdDialog } from '@angular/material';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 // UrlDialog
 import { UrlDialog } from '../dialogs/urldialog.component';
 @Injectable()
 export class UrlDialogService {
     settings: any = JSON.parse(localStorage.getItem('settings')) || {};
-    constructor(private snackbar: MdSnackBar, private dialog: MdDialog) { }
+    constructor(private snackbar: MatSnackBar, private dialog: MatDialog) { }
     /**
-     * Goes to a url
+     * Opens a dialog which asks the user whether to allow the app to redirect user to Uthe specified URL
      * @param {string} url The url to go to
-     * @return void
      */
-    public goToUrl(url: string): void {
+    public goToUrl(url: string) {
         if (this.settings.resetWarnings) {
             let dialogRef = this.dialog.open(UrlDialog);
             dialogRef.componentInstance.url = url;
             dialogRef.afterClosed().subscribe(result => {
                 if (result == 'cancel') {
                     // Do nothing
-                    this.snackbar.open('You cancelled the redirect', null, { duration: 5000 });
+                    this.snackbar.open('Redirect was cancelled', null, { duration: 5000, horizontalPosition: "start", extraClasses: ["mat-elevation-z2"] });
                 } else if (result == 'redirect') {
                     if (this.settings.openNewTab) {
                         console.debug('Opening ' + url + ' in a new tab.');

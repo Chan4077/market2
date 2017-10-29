ab8c583b86d6aca63a799ced39303ac26c65986f
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 01:15:13 +0800

    refactor: Process of converting to mat prefix [6]

--- src/app/market.module.ts
+++ src/app/market.module.ts
@@ -9,35 +9,66 @@
 
 // NgModule
 import { NgModule } from '@angular/core';
-// MaterialModules to import
-import { MdInputModule, MdDialogModule, MdTooltipModule, MdButtonModule, MdIconModule, MdTabsModule, MdCardModule, OverlayModule, MdProgressSpinnerModule, MdSnackBarModule, MdMenuModule, MdCheckboxModule, MdListModule, MdRippleModule, StyleModule, A11yModule, PlatformModule, CompatibilityModule, ObserveContentModule, MdSidenavModule, MdToolbarModule, MdDatepickerModule, MdNativeDateModule, MdSliderModule, MdSelectModule, MdOptionModule, MdChipsModule } from '@angular/material';
-// List of components to import
-const MARKET_MATERIAL_MODULES = [
-    MdSliderModule,
-    MdInputModule,
-    MdDialogModule,
-    MdTooltipModule,
-    MdButtonModule,
-    MdIconModule,
-    MdTabsModule,
-    MdCardModule,
-    OverlayModule,
-    MdProgressSpinnerModule,
-    MdSnackBarModule,
-    MdMenuModule,
-    MdCheckboxModule,
-    MdListModule,
-    MdRippleModule,
-    MdSidenavModule,
-    MdToolbarModule,
-    MdDatepickerModule,
-    MdNativeDateModule,
-    MdSelectModule,
-    MdOptionModule,
-    MdChipsModule
-]
+//#region Material imports
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+//#endregion
+//#region CDK imports
+import { A11yModule } from '@angular/cdk/a11y';
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
+//#endregion
+//#region Material modules required by the app
+const MATERIAL_MODULES = [
+	MatButtonModule,
+	MatCardModule,
+	MatCheckboxModule,
+	MatChipsModule,
+	MatDatepickerModule,
+	MatDialogModule,
+	MatFormFieldModule,
+	MatIconModule,
+	MatInputModule,
+	MatListModule,
+	MatMenuModule,
+	MatNativeDateModule,
+	MatProgressSpinnerModule,
+	MatSelectModule,
+	MatSnackBarModule,
+	MatSidenavModule,
+	MatTabsModule,
+	MatToolbarModule,
+	MatTooltipModule
+];
+//#endregion
+//#region CDK modules required by the app
+const CDK_MODULES = [
+	A11yModule,
+	LayoutModule,
+	OverlayModule
+];
+//#endregion
 @NgModule({
-    exports: MARKET_MATERIAL_MODULES
+    exports: [
+		MATERIAL_MODULES,
+		CDK_MODULES
+	]
 })
-// Export the module
 export class MarketMaterialModule {}

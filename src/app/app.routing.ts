e00b5aed0c8937ccd795de5d2776ffce827cc867
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 02:46:24 +0800

    refactor: Use tab indent instead of two spaces [2]

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { TestpageComponent } from './testpage/testpage.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // App components
@@ -7,32 +8,30 @@ import { MarketComponent, NewestMarketSortComponent, PopularMarketSortComponent 
 import { AccountComponent } from './account.component';
 /**
  * Routes used for routing the app
- * @version 1.0.2
- * @author Edric Chan
- * @example In your `app.module.ts`, import `routing` into `imports` in `NgModule`
- * @description Paths to route to with components
- * @type Routes[]
  */
 export const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'market', component: MarketComponent, children: [
-    {
-      path: 'sort',
-      children: [
-        {
-          path: 'newest',
-          component: NewestMarketSortComponent
-        },
-        {
-          path: 'popular',
-          component: PopularMarketSortComponent
-        }
-      ]
-    }
-  ]},
-  { path: 'account', component: AccountComponent},
-  { path: '', redirectTo: '/market', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent }
+	{ path: 'about', component: AboutComponent },
+	{
+		path: 'market', component: MarketComponent, children: [
+			{
+				path: 'sort',
+				children: [
+					{
+						path: 'newest',
+						component: NewestMarketSortComponent
+					},
+					{
+						path: 'popular',
+						component: PopularMarketSortComponent
+					}
+				]
+			}
+		]
+	},
+	{ path: 'account', component: AccountComponent },
+	{ path: 'test', component: TestpageComponent },
+	{ path: '', redirectTo: '/market', pathMatch: 'full' },
+	{ path: '**', component: PageNotFoundComponent }
 ];
 
 export const AppRouting: ModuleWithProviders = RouterModule.forRoot(routes);

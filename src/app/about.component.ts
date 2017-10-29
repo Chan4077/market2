c13f0909af6405797402cf67ccc63ef280cae45b
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 01:03:34 +0800

    feat: Add about page
    refactor: Process of converting to mat prefix [1]

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'about-component',
-  templateUrl: './about.component.html'
+	selector: 'about-component',
+	templateUrl: './about.component.html'
 })
 
 export class AboutComponent {
-    // Not much here
+	links = [
+		{
+			title: "Google+",
+			link: "https://plus.google.com/+EdricChan03"
+		},
+		{
+			title: "Facebook",
+			link: "https://facebook.com/Chan4077"
+		},
+		{
+			title: "Instagram",
+			link: "https://instagram.com/chanedric"
+		},
+		{
+			title: "Github",
+			link: "https://github.com/Chan4077"
+		},
+		{
+			title: "Twitter",
+			link: "https://twitter.com/EdricChan03"
+		}
+	]
 }

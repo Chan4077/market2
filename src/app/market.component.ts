f6c722a400a86afddc95126a13ecd4ce8c9b6d8f
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 01:23:43 +0800

    fix: Add missing functions
    - Remove commented code
    - Remove return statements of `void`
    - refactor: Process of converting to mat prefix [8]

--- src/app/market.component.ts
+++ src/app/market.component.ts
@@ -1,5 +1,7 @@
+import { DomSanitizer } from '@angular/platform-browser';
+import { Shared } from './shared';
 import { Component, OnInit } from '@angular/core';
-import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 // Marketitem service
 import { MarketItemService } from './services/marketitem.service';
 import { Item } from './marketitems';
@@ -7,135 +9,151 @@ import { MoreInfoDialog } from './dialogs/moreinfo.component';
 // Angularfire2
 // import { AngularFire } from 'angularfire2';
 @Component({
-    selector: 'market-sort-nav',
-    templateUrl: './market-sort-nav.component.html'
+	selector: 'market-sort-nav',
+	templateUrl: './market-sort-nav.component.html'
 })
 export class MarketNavComponent {
-    sortLinks = [
-        { label: 'Newest', link: 'sort/newest' },
-        { label: 'Popular', link: 'sort/popular' },
-        { label: 'Starred', link: 'sort/starred' }
-    ]
+	sortLinks = [
+		{ label: 'Newest', link: 'sort/newest' },
+		{ label: 'Popular', link: 'sort/popular' },
+		{ label: 'Starred', link: 'sort/starred' }
+	]
 }
 @Component({
-    selector: 'market',
-    templateUrl: './market.component.html'
+	selector: 'market',
+	templateUrl: './market.component.html'
 })
 export class MarketComponent implements OnInit {
-    marketItems: Item[];
-    newCommentHidden: any = [];
-    commentHidden: any = [];
-    constructor(private marketItemService: MarketItemService, public dialog: MdDialog/*, private af: AngularFire*/) { }
+	marketItems: Item[];
+	newCommentHidden: any = [];
+	commentHidden: any = [];
+	constructor(private marketItemService: MarketItemService, private dialog: MatDialog, private shared: Shared, private dom: DomSanitizer) { }
 
-    getMarketItems(): void {
-        this.marketItemService.getMarketItems().then(marketItems => this.marketItems = marketItems);
-    }
-    ngOnInit(): void {
-        this.getMarketItems();
-    }
-    viewAuthor($event: any): void {
-        let name = JSON.stringify($event);
-        // let config = new MdDialogConfig();
-        let dialogRef = this.dialog.open(MoreInfoDialog);
-        dialogRef.componentInstance.json = name;
-        // dialogRef.componentInstance.data = author;
-    }
-    showNewComment(index: any, showOrHide?: boolean) {
-        if (showOrHide) {
-            if (showOrHide) {
-                this.newCommentHidden[index] = true;
-            } else {
-                this.newCommentHidden[index] = false;
-            }
-        } else {
-            if (this.newCommentHidden[index]) {
-                this.newCommentHidden[index] = false;
-            } else {
-                this.newCommentHidden[index] = true;
-            }
-        }
-    }
-    showComment(index: any) {
-        if (this.commentHidden[index]) {
-            this.commentHidden[index] = false;
-        } else {
-            this.commentHidden[index] = true;
-        }
-    }
-    reply(comment: any, item: any) {
-        let dialogRef = this.dialog.open(ReplyDialog);
-        dialogRef.componentInstance.comment = comment;
-        dialogRef.componentInstance.item = item;
-        dialogRef.afterClosed().subscribe(result => {
-            /** @todo Update this */
-            console.log(result);
-        })
-    }
-    newCommentIsHidden(index: any) {
-        if (this.newCommentHidden[index]) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    commentIsHidden(index: any) {
-        if (this.commentHidden[index]) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	getMarketItems() {
+		this.marketItemService.getMarketItems().then(marketItems => this.marketItems = marketItems);
+	}
+	ngOnInit() {
+		this.getMarketItems();
+	}
+	viewAuthor($event: any) {
+		let name = JSON.stringify($event);
+		let dialogRef = this.dialog.open(MoreInfoDialog);
+		dialogRef.componentInstance.json = name;
+		// dialogRef.componentInstance.data = author;
+	}
+	reportAbuse(item) {
+		let dialogRef = this.shared.openPromptDialog({ title: "Report Abuse", msg: this.dom.bypassSecurityTrustHtml(`<p>You're reporting an issue for <strong>"${item.name}"</strong> from ${item.user}.</p> <p>What's the issue? Type it in the input field below this text.</p>`), color: "primary", placeholder: "Issue", textarea: true, isHtml: true, disableClose: true });
+		dialogRef.afterClosed().subscribe(result => {
+			console.log(result);
+			if (result == 'cancel') {
+				// User cancelled
+			} else {
+				this.shared.openSnackBar({ msg: "Your issue was sent! Your issue will be reviewed and taken upon action if required.", action: "Undo", additionalOpts: { duration: 7000, horizontalPosition: "start", extraClasses: ["mat-elevation-z2"] } });
+			}
+		})
+	}
+	showFewerPosts(user: string) {
+		this.shared.openSnackBar({ msg: `Fewer posts will be shown from ${user} and will take effect from the next time you login.`, action: "Undo", additionalOpts: { duration: 7000, horizontalPosition: "start", extraClasses: ["mat-elevation-z2"] } });
+	}
+	blockUser(user: string) {
+		this.shared.openSnackBar({ msg: `${user} was successfully blocked and you will no longer see any posts from ${user}.`, action: "Undo", additionalOpts: { duration: 7000, horizontalPosition: "start", extraClasses: ["mat-elevation-z2"] } });
+	}
+	showNewComment(index: any, showOrHide?: boolean) {
+		if (showOrHide) {
+			if (showOrHide) {
+				this.newCommentHidden[index] = true;
+			} else {
+				this.newCommentHidden[index] = false;
+			}
+		} else {
+			if (this.newCommentHidden[index]) {
+				this.newCommentHidden[index] = false;
+			} else {
+				this.newCommentHidden[index] = true;
+			}
+		}
+	}
+	showComment(index: any) {
+		if (this.commentHidden[index]) {
+			this.commentHidden[index] = false;
+		} else {
+			this.commentHidden[index] = true;
+		}
+	}
+	reply(comment: any, item: any) {
+		let dialogRef = this.dialog.open(ReplyDialog);
+		dialogRef.componentInstance.comment = comment;
+		dialogRef.componentInstance.item = item;
+		dialogRef.afterClosed().subscribe(result => {
+			/** @todo Update this */
+			console.log(result);
+		})
+	}
+	newCommentIsHidden(index: any) {
+		if (this.newCommentHidden[index]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	commentIsHidden(index: any) {
+		if (this.commentHidden[index]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
     /**
      * Posts a comment
      * @version 1.2.2
      * @param {string} comment The comment to post
      */
-    postComment(comment: string) {
-        console.log(comment);
-    }
+	postComment(comment: string) {
+		console.log(comment);
+	}
 }
 @Component({
-    selector: 'sort-popular-market',
-    templateUrl: './market-sort/popularsort.component.html'
+	selector: 'sort-popular-market',
+	templateUrl: './market-sort/popularsort.component.html'
 })
 export class PopularMarketSortComponent { }
 
 @Component({
-    selector: 'sort-newest-market',
-    templateUrl: './market-sort/newestsort.component.html'
+	selector: 'sort-newest-market',
+	templateUrl: './market-sort/newestsort.component.html'
 })
 export class NewestMarketSortComponent { }
 
 @Component({
-    selector: 'reply-dialog',
-    template: `
-            <h2 md-dialog-title>Reply to {{comment.author}}</h2>
-            <md-dialog-content>
-                <md-list>
-                    <md-list-item>
-                        <h4 md-line>{{comment.author}}</h4>
-                        <p md-line>{{comment.comment}}</p>
-                    </md-list-item>
-                </md-list>
+	selector: 'reply-dialog',
+	template: `
+            <h2 matDialogTitle>Reply to {{comment.author}}</h2>
+            <mat-dialog-content>
+                <mat-list>
+                    <mat-list-item>
+                        <h4 matLine>{{comment.author}}</h4>
+                        <p matLine>{{comment.comment}}</p>
+                    </mat-list-item>
+                </mat-list>
                 <input [(ngModel)]="newCommentReply.id" type="hidden">
-                <md-input-container color="primary">
-                    <input mdInput [(ngModel)]="newCommentReply.value">
-                </md-input-container>
-            </md-dialog-content>
-            <md-dialog-actions align="end">
-                <button md-button md-dialog-close color="primary">Cancel</button>
-                <button md-button (click)="dialogRef.close(newCommentReply)" color="primary">Reply</button>
-            </md-dialog-actions>
+                <mat-form-field color="primary">
+                    <input matInput [(ngModel)]="newCommentReply.value">
+                </mat-form-field>
+            </mat-dialog-content>
+            <mat-dialog-actions align="end">
+                <button mat-button matDialogClose color="primary">Cancel</button>
+                <button mat-button (click)="dialogRef.close(newCommentReply)" color="primary">Reply</button>
+            </mat-dialog-actions>
             `
 })
-export class ReplyDialog implements OnInit{
-    comment: any;
-    item: any;
-    newCommentReply: any;
-    constructor(public dialogRef: MdDialogRef<ReplyDialog>) {}
-    ngOnInit(){
-        this.comment = this.dialogRef.componentInstance.comment;
-        this.item = this.dialogRef.componentInstance.item;
-        this.newCommentReply = {id: this.item.id, value: '', replyTo: ''};
-    }
+export class ReplyDialog implements OnInit {
+	comment: any;
+	item: any;
+	newCommentReply: any;
+	constructor(public dialogRef: MatDialogRef<ReplyDialog>) { }
+	ngOnInit() {
+		this.comment = this.dialogRef.componentInstance.comment;
+		this.item = this.dialogRef.componentInstance.item;
+		this.newCommentReply = { id: this.item.id, value: '', replyTo: '' };
+	}
 }

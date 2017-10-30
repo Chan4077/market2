85ecec175133f08656529069cb3c625d380e4099
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 16:45:07 +0800

    refactor: Remove JSDoc

--- src/app/market.module.ts
+++ src/app/market.module.ts
@@ -1,12 +1,3 @@
-/**
- * This module is meant for imports for ngMaterial so that the amount of code to import is reduced
- * @version 1.1.0
- * @example Import into <code>app.module.ts</code> as <code>MarketMaterialModule</code>
- * @author Edric Chan
- * @description Based on this PR which is currently not merged (this PR will depreceate <code>MaterialModule</code>):
- * https://github.com/angular/material2/pull/3840
- */
-
 // NgModule
 import { NgModule } from '@angular/core';
 //#region Material imports

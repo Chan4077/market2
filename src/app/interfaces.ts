3171cea61c97b6622959d107115bfc9f92e9693d
Edric Chan <edric.chan.1997@gmail.com>
2017-10-30 17:16:18 +0800

    refactor(interfaces): Add JSDoc

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,7 +1,24 @@
 export interface ItemComment {
+	/**
+	 * The author of the comment
+	 * @type {string}
+	 */
 	author: string;
+	/**
+	 * The comment itself
+	 * @type {string}
+	 */
 	comment: string;
+	/**
+	 * The avatar image URL of the comment
+	 * @type {string}
+	 */
 	avatar?: string;
+	/**
+	 * The ID of the comment
+	 * @todo Remove this property
+	 * @type {number}
+	 */
 	id?: number;
 }
 
@@ -63,16 +80,58 @@ export interface Item {
 	comments: ItemComment[];
 }
 export interface Settings {
+	/**
+	 * Whether dark theme is enabled
+	 * @type {boolean}
+	 */
 	isDarkTheme: boolean;
+	/**
+	 * The name of the currently logged-in user
+	 * @type {string}
+	 */
 	name: string;
+	/**
+	 * The email of the currently logged-in user
+	 * @type {string}
+	 */
 	email: string;
+	/**
+	 * The birthday of the currently logged-in user
+	 * @type {Date|any}
+	 */
 	birthday: Date | any;
+	/**
+	 * Whether to enable developer settings
+	 * @todo Add basic functionality
+	 * @type {boolean}
+	 */
 	showDeveloper: boolean;
+	/**
+	 * Whether to show the app's logo with a progress spinner on load
+	 * @todo Rename this property
+	 * @type {boolean}
+	 */
 	showGreeting: boolean;
 }
 export interface Links {
-    name: string;
-    description: string;
-    icon: string;
-    href: string;
+	/**
+	 * The name of the link
+	 * @type {string}
+	 */
+	name: string;
+	/**
+	 * The description of the link
+	 * @type {string}
+	 */
+	description: string;
+	/**
+	 * The icon of the link
+	 * @type {string}
+	 */
+	icon?: string;
+	/**
+	 * The href of the link
+	 * @type {string}
+	 */
+	href: string;
 }

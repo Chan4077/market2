67a05de88fe29f457c90aef7a6df83b67cff2fca
Edric Chan <edric.chan.1997@gmail.com>
2017-11-01 00:22:00 +0800

    refactor: Use tab indent instead of two spaces [2]

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
-  selector: 'page-not-found',
-  templateUrl: './page-not-found.component.html'
+	selector: 'page-not-found',
+	templateUrl: './page-not-found.component.html'
 })
 
 export class PageNotFoundComponent {
-  constructor(private router: Router) {}
-  refresh(): void {
-    window.location.reload(true);
-  }
+	constructor(private router: Router) { }
+	refresh(): void {
+		window.location.reload(true);
+	}
 }

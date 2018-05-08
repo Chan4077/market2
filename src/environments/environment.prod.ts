d971179fcdb3ce170083a5e048db9618ed38b39a
Edric Chan <edric.chan.1997@gmail.com>
2018-05-08 16:34:15 +0800

    chore(environment): add production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 export const environment = {
-	production: false,
+	production: true,
 	firebase: {
 		apiKey: 'AIzaSyAi79KeFQmrFeG51iWkCWQRCKG396gI5js',
 		authDomain: 'market2beta.firebaseapp.com',
